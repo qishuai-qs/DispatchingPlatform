f74d9723949282faf5ada88437f1680a1544b211
qishuai <shuai7868@gmail.com>
2026-05-07 18:07:43 +0800

    1

--- app/api/api.py
+++ app/api/api.py
@@ -4,7 +4,7 @@ API路由聚合模块
 
 from fastapi import APIRouter
 
-from app.api.v1 import auth, system, tasks, users
+from app.api.v1 import auth, system, tasks, templates, users
 
 # 创建v1版本路由
 api_router = APIRouter(prefix="/api/v1")
@@ -13,5 +13,6 @@ api_router = APIRouter(prefix="/api/v1")
 api_router.include_router(auth.router)
 api_router.include_router(users.router)
 api_router.include_router(tasks.router)
+api_router.include_router(templates.router)
 api_router.include_router(system.router)
 
